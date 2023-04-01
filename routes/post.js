1828fb7c63e6fe9c93fa6562965c5307e2a6d0fe
AustynYou <austyn.you@gmail.com>
2023-03-31 21:30:13 -0700

    🚧 Implement retweet, undoRetweet(removePost)

--- routes/post.js
+++ routes/post.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 
 const { addPost, editPost, removePost, 
   addComment, editComment, removeComment,
-  addLike, removeLike,
-  uploadImage } = require("../controllers/post");
+  addLike, removeLike, uploadImage,
+  retweet } = require("../controllers/post");
 const { isLoggedIn } = require("../controllers/middlewares");
 
 const router = Router();
@@ -17,6 +17,22 @@ try {
   console.log('uploads 폴더가 없으므로 생성합니다.');
   fs.mkdirSync('uploads');
 }
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads')
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname, ext);
+      done(null, basename + '_' + new Date().getTime() + ext); 
+    }
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 }
+})
+
+router.post('/images', isLoggedIn, upload.array('image'), uploadImage);
 
 router.post('/', isLoggedIn, addPost);
 router.patch('/:postId', isLoggedIn, editPost);
@@ -29,20 +45,7 @@ router.delete('/:postId/comment/:commentId', isLoggedIn, removeComment);
 router.post('/:postId/like', isLoggedIn, addLike);
 router.delete('/:postId/like', isLoggedIn, removeLike); 
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads')
-    },
-    filename(rea, file, done) {
-      const ext = path.extname(file.originalname);
-      const basename = path.basename(file.originalname, ext);
-      done(null, basename + new Date + ext); 
-    }
-  }),
-  limits: { fileSize: 20 * 1024 * 1024 }
-})
-
-router.post('/images', isLoggedIn, upload.array('image'), uploadImage);
+router.post('/:postId/retweet', isLoggedIn, retweet);
+router.delete('/:postId/retweet', isLoggedIn, removePost); 
 
 module.exports = router;
