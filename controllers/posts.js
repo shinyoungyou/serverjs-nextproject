5b3abcb469c729283886be0727d1320218c98aa3
AustynYou <austyn.you@gmail.com>
2023-04-01 11:47:36 -0700

    🚧 loadPosts: lastId & limit

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,59 +1,64 @@
+const { Op } = require('sequelize'); 
 const { User, Post, Comment, Hashtag, Image } = require('../models');
 
 exports.loadPosts = async (req, res, next) => {
-  const allPosts = await Post.findAll({
-    // where: {
-    //   id: lastId
-    // },
-    limit: 10,
-    order: [
-      ['createdAt', 'DESC'],
-      [Comment, 'createdAt', 'DESC'],
-    ],
-    include: [{
-      model: Image,
-      attributes: ['id', 'src', 'alt', 'PostId']
-    },{
-      model: User,
-      attributes: ['id', 'username']
-    }, {
-      model: Comment,
-      include: [{
-        model: User,
-        attributes: ['id', 'username']
-      }]
-    }, {
-      model: User,
-      as: 'Likers',
-      attributes: ['id']
-    },{
-      model: Post,
-      as: 'Retweet',
+  try {
+    console.log("asdf");
+    const where = {};
+    const lastId = parseInt(req.query.lastId);
+    console.log(lastId);
+    if (lastId) { // 초기 로딩이 아닐 때
+      where.id = { [Op.lt]: lastId }
+    }
+    const tenPosts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC'],
+      ],
       include: [{
         model: Image,
         attributes: ['id', 'src', 'alt', 'PostId']
       },{
-          model: User, // post author
+        model: User,
+        attributes: ['id', 'username']
+      }, {
+        model: Comment,
+        include: [{
+          model: User,
           attributes: ['id', 'username']
+        }]
+      }, {
+        model: User,
+        as: 'Likers',
+        attributes: ['id']
       },{
-          model: Comment,
-          include: [{
-            model: User, // comment author
+        model: Post,
+        as: 'Retweet',
+        include: [{
+          model: Image,
+          attributes: ['id', 'src', 'alt', 'PostId']
+        },{
+            model: User, // post author
             attributes: ['id', 'username']
-          }]
-      },{
-          model: User, // liker
-          as: 'Likers',
-          attributes: ['id']
-      }]
-    },]
-  });
-  if (!allPosts){
-    return res.status(404).send('There is no post yet.');
-  }
-  try {
-    // console.log(req.body);
-    return res.status(200).json(allPosts);
+        },{
+            model: Comment,
+            include: [{
+              model: User, // comment author
+              attributes: ['id', 'username']
+            }]
+        },{
+            model: User, // liker
+            as: 'Likers',
+            attributes: ['id']
+        }]
+      },]
+    }); 
+    // if (!allPosts){
+    //   return res.status(404).send('There is no post yet.');
+    // }
+    return res.status(200).json(tenPosts);
   } catch (error) {
     console.error(error);
     return next(error);
