39b74a29d448b5554a7b6046a2bc49872ffbdaaa
AustynYou <austyn.you@gmail.com>
2023-04-07 20:39:29 -0700

    🔧 Fix Err: Dialect undefined

--- config/config.js
+++ config/config.js
@@ -14,15 +14,17 @@ const conf = {
   test: {
     username: "root",
     password: process.env.DB_PASSWORD,
-    database: "database_test",
+    database: "next_db",
     host: "127.0.0.1",
+    port: 8889,
     dialect: "mysql"
   },
   production: {
     username: "root",
     password: process.env.DB_PASSWORD,
-    database: "database_production",
+    database: "next_db",
     host: "127.0.0.1",
+    port: 8889,
     dialect: "mysql"
   }
 }
