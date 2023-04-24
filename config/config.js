d4a52f0415197e1e688984150d8a3a81f81c2802
AustynYou <austyn.you@gmail.com>
2023-04-23 22:56:54 -0700

    🔥 Undo Fix

--- config/config.js
+++ config/config.js
@@ -9,8 +9,7 @@ const conf = {
     database: "next_db",
     host: "127.0.0.1",
     port: 8889,
-    dialect: "mysql",
-    socketPath: 31026
+    dialect: "mysql"
   },
   test: {
     username: "root",
